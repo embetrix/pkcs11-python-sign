ea2129f64f070dcf8c972396317a2404bd1e8ee3
Ayoub Zaki <ayoub.zaki@embetrix.com>
2023-04-29 19:48:17 +0200

    initial commit

---
+++ pkcs11-sign.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+#
+# works only with NitroKey
+# export PKCS11_MODULE=/usr/lib/x86_64-linux-gnu/opensc-pkcs11.so
+# pkcs11-tool --keypairgen --key-type EC:prime256v1 --label "testkeyEC3333" --id 3333  --login --usage-sign
+#
+
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import ECC
+from Crypto.Signature import DSS
+
+from pkcs11 import KeyType, ObjectClass, Mechanism
+from pkcs11.util.ec import encode_ec_public_key
+
+import os
+import pkcs11
+
+message = b'I give my permission to order #4355'
+
+lib = pkcs11.lib(os.environ['PKCS11_MODULE'])
+token = lib.get_token(token_label='SmartCard-HSM (UserPIN)')
+
+print("sign:")
+with token.open(rw=True, user_pin='123456') as session:
+    priv   = session.get_key(label='testkeyEC3333', key_type=KeyType.EC, object_class=ObjectClass.PRIVATE_KEY)
+    signature = priv.sign(message, mechanism=Mechanism.ECDSA_SHA256)
+
+print("verify:")
+with token.open(rw=True, user_pin='123456') as session:
+    pubkey = session.get_key(label='testkeyEC3333', key_type=KeyType.EC, object_class=ObjectClass.PUBLIC_KEY)
+    h = SHA256.new(message)
+    verifier = DSS.new(ECC.import_key(encode_ec_public_key(pubkey)), 'fips-186-3')
+    try:
+        verifier.verify(h, signature)
+        print("signature ok.")
+    except ValueError:
+        print("signature not ok!")
